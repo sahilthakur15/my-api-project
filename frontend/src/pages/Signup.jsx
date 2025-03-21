caf7e8a8acb5a898d21a68acfc829d9636c993cc
sahilthakur15 <sahilthakurwins@gmail.com>
2025-03-21 17:14:25 +0530

    fix: axios instance chnges

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { signupUser } from "../utils/axiosInstance"; // Import signup API function
 
